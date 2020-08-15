436cf40f9a700c1d8b8a898426778cc915803603
Anthony Alcorn <xmastony@gmail.com>
2020-08-15 01:20:38 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do
-    puts array[count] == value_to_find
+    if array[count] == value_to_find
+      return count
       count += 1 
     end
 end
